4aa28cbb1ed5853d13bbbe034618a790ed76baca
NikitaRepin-QA <88780189+repinikita@users.noreply.github.com>
2023-02-03 23:14:10 +0500

    new battleship.js

--- battleship.js
+++ battleship.js
@@ -1,16 +1,17 @@
-var randomLoc = Math.floor (Math.random()*5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess;
-var hits = 0;
-var guesses = 0;
-var mistakes = 0;
-var isSunk = false;
+let randomLoc = Math.floor (Math.random()*5);
+let location1 = randomLoc;
+let location2 = location1 + 1;
+let location3 = location2 + 1;
+let guess;
+let hits = 0;
+let guesses = 0;
+let mistakes = 0;
+let isSunk = false;
 
-while (isSunk == false) {
-	guess = prompt("Привет! Это мини игра морской бой :) Представь, что в 7 ячейках по горизонтали ••••••• (от нуля до шести) расположен 3-ёх палубный корабль врага. Твоя задача потопить его за меньшее кол-во попыток. Ты готов выстрелить? Стреляй! (введи число 0-6):");
-	if (guess <0 || guess >6) {
+
+while (!isSunk) {
+	guess = prompt("Привет! Это мини-игра морской бой :) Представь, что в 7 ячейках по горизонтали ••••••• (от нуля до шести) расположен 3-ёх палубный корабль врага. Твоя задача потопить его за меньшее кол-во попыток. Ты готов выстрелить? Стреляй! (введи число 0-6):");
+	if (isNaN(guess) || guess <0 || guess >6 || guess == "" || guess == " ") {
 	 	alert(" Введи значение 0-6!");
 	} 	else if (guess == null){
 		 alert("Жаль, что вы покинули игру :(");
@@ -18,7 +19,6 @@ while (isSunk == false) {
 	}
 		else {
 		guesses = guesses + 1;
-
 
 		if ( guess == location1 || guess == location2 || guess == location3) {
 			alert( " ПОПАЛ!");
@@ -28,14 +28,15 @@ while (isSunk == false) {
 				alert("Корабль потоплен!");
 
 			}
-		} else {
+
+		}
+		 else {
 			alert("МИМО");
+			mistakes++;
+		
 		}
 	}
+	if (isSunk) alert("Поздравляем, вы потопили корабль, ваша оценка " + (10 - mistakes) + "/10!");
 }
 
- var status = ("Поздравляем, вы потопили корабль, ваша оценка " + (10 - mistakes) + "/10!");
- alert(status);		
 
-
- //alert("Вы уже отгадали это число, попробуйте снова");	
